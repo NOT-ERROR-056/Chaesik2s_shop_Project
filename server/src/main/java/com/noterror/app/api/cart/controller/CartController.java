c835921a5ccfb8d92480503a4a9ee7be05f787b9
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-06 20:25:58 +0900

    [docs] 장바구니 관련 API 문서 작성

--- server/src/main/java/com/noterror/app/api/cart/controller/CartController.java
+++ server/src/main/java/com/noterror/app/api/cart/controller/CartController.java
@@ -13,6 +13,9 @@ import com.noterror.app.api.entity.cart.CartDetail;
 import com.noterror.app.api.entity.member.Member;
 import com.noterror.app.api.global.response.SingleCartDetailResponse;
 import com.noterror.app.api.global.response.SingleCartResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Api(tags = {"FRONT-OFFICE", "CART"})
 @RestController
 @RequestMapping(value = "/cart")
 @RequiredArgsConstructor
@@ -35,9 +39,10 @@ public class CartController {
     /**
      * 장바구니 제품 추가
      */
+    @ApiOperation(value = "장바구니 제품 추가 API", notes = "제품 ID에 해당하는 제품을 장바구니에 추가합니다.")
     @PostMapping("/product/{product-id}")
-    public ResponseEntity addProductInCart(@PathVariable("product-id") Long productId,
-                                           @RequestBody @Valid AddProductInCartDto addProductInCartDto) {
+    public ResponseEntity<SingleCartDetailResponse<CartDetail>> addProductInCart(@ApiParam(value = "장바구니 ID", required = true, example = "1") @PathVariable("product-id") Long productId,
+                                           @ApiParam(value = "선택한 제품의 수량") @RequestBody @Valid AddProductInCartDto addProductInCartDto) {
         CartDetail cartDetail = toCartDetailByProduct(productId, addProductInCartDto);
         Cart cart = cartService.addProductInCart(cartDetail);
         CartDetailResponseDto response = new CartDetailResponseDto(cart);
@@ -49,8 +54,9 @@ public class CartController {
     /**
      * 장바구니 제품 전체 조회
      */
+    @ApiOperation(value = "장바구니에 제품 전체 조회", notes = "장바구니 내역을 전체 조회합니다.")
     @GetMapping
-    public ResponseEntity getCartDetailsInCart() {
+    public ResponseEntity<SingleCartResponse<Cart>> getCartDetailsInCart() {
         Cart cartOfMember = cartService.getCartOfMember(getMemberByEmail());
         CartResponseDto response = new CartResponseDto(cartOfMember);
         return new ResponseEntity(
@@ -60,9 +66,10 @@ public class CartController {
     /**
      * 장바구니 제품 수량 변경
      */
+    @ApiOperation(value = "장바구니에 제품 수량 변경", notes = "장바구니에 제품 ID에 해당하는 수량을 변경합니다.")
     @PutMapping("/cart-detail/{cart-detail-id}")
-    public ResponseEntity updateCartProduct(@PathVariable("cart-detail-id") Long cartDetailId,
-                                            @RequestBody @Valid CartDetailUpdateInfoDto cartDetailUpdateInfoDto) {
+    public ResponseEntity<SingleCartDetailResponse<CartDetail>> updateCartProduct(@ApiParam(value = "장바구니에 제품 ID", required = true, example = "1") @PathVariable("cart-detail-id") Long cartDetailId,
+                                            @ApiParam(value = "변경 수량") @RequestBody @Valid CartDetailUpdateInfoDto cartDetailUpdateInfoDto) {
         cartDetailUpdateInfoDto.setCartDetailId(cartDetailId);
         Cart updateCart = cartService.updateCartDetail(cartDetailUpdateInfoDto);
         CartDetailResponseDto response = new CartDetailResponseDto(updateCart);
@@ -73,8 +80,9 @@ public class CartController {
     /**
      * 장바구니 제품 삭제
      */
+    @ApiOperation(value = "장바구니에 제품 삭제", notes = "장바구니에 제품 ID에 해당하는 제품을 삭제합니다.")
     @DeleteMapping("cart-detail/{cart-detail-id}")
-    public ResponseEntity deleteCartProduct(@PathVariable("cart-detail-id") Long cartDetailId) {
+    public ResponseEntity deleteCartProduct(@ApiParam(value = "장바구니에 제품 ID", required = true, example = "1") @PathVariable("cart-detail-id") Long cartDetailId) {
         cartService.deleteCart(cartDetailId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
