3143125cc6a01c230fd317b14f81653dd5bfc350
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-06 17:57:48 +0900

    [refactor] 채식가능 유형 필드 오타 및 map 메서드 refactoring
    1) eatableVegetarian 필드 오타 수정
    2) stream.map 메서드 단축

--- server/src/main/java/com/noterror/app/api/global/response/SingleProductResponse.java
+++ server/src/main/java/com/noterror/app/api/global/response/SingleProductResponse.java
@@ -1,10 +1,7 @@
 package com.noterror.app.api.global.response;
 
-import com.noterror.app.api.entity.Product;
 import com.noterror.app.api.entity.Vegetarian;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
 import lombok.Getter;
 
 import java.util.List;
@@ -16,12 +13,12 @@ public class SingleProductResponse<T> {
     private T product;
 
     @ApiModelProperty(notes = "제품을 섭취할 수 있는 유형", example = "비건")
-    private List<String> eatableVegetarain;
+    private List<String> eatableVegetarian;
 
     public SingleProductResponse(T product, List<Vegetarian> eatableVegetarian) {
         this.product = product;
-        this.eatableVegetarain = eatableVegetarian.stream()
-                .map(v -> v.getVegetarianType())
+        this.eatableVegetarian = eatableVegetarian.stream()
+                .map(Vegetarian::getVegetarianType)
                 .collect(Collectors.toList());
     }
 
